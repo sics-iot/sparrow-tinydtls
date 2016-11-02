5b2275e20f7ab707a0b9fc398a483138739ba9c6
Ang, Chee Hong <chee.hong.ang@intel.com>
2016-11-02 11:04:03 +0800

    Updated to new Contiki definitions
    Signed-off-by: Ang, Chee Hong <chee.hong.ang@intel.com>

--- dtls_debug.c
+++ dtls_debug.c
@@ -162,7 +162,7 @@ dsrv_print_addr(const session_t *addr, char *buf, size_t len) {
 #else /* HAVE_ARPA_INET_H */
 # if WITH_CONTIKI
   char *p = buf;
-#  ifdef UIP_CONF_IPV6
+#  ifdef NETSTACK_CONF_WITH_IPV6
   uint8_t i;
   const char hex[] = "0123456789ABCDEF";
 
@@ -181,12 +181,12 @@ dsrv_print_addr(const session_t *addr, char *buf, size_t len) {
     *p++ = hex[(addr->addr.u8[i+1] & 0x0f)];
   }
   *p++ = ']';
-#  else /* UIP_CONF_IPV6 */
+#  else /* NETSTACK_CONF_WITH_IPV6 */
 #   warning "IPv4 network addresses will not be included in debug output"
 
   if (len < 21)
     return 0;
-#  endif /* UIP_CONF_IPV6 */
+#  endif /* NETSTACK_CONF_WITH_IPV6 */
   if (buf + len - p < 6)
     return 0;
 
